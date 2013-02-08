a7c40f3226363731446938d67a32d7227f285685
E. Troy Clement <eclement@nmsu.edu>
2013-02-07 22:51:07 -0700

    Set up Fey and started VeiwManager
    I set up Fey with the main display. I started working on VeiwManager,
    but hit a road-block when I couldn't figure out how to import it. It
    keeps saying: "ImportError: No module named VeiwManager", but it's in
    the same directory!
    I give up. Fey runs fine for now.

--- Fey.py
+++ Fey.py
@@ -1,13 +1,65 @@
 #Main Runner
 #bring in all the views
 #add current view to viewmanager
-#
+
+#Starting code ripped from: http://pygametutorials.wikidot.com/tutorials-basic
+
+import pygame
+from pygame.locals import *
+
+#import VeiwManager         #Can't get this to work!!!
+#v = VeiwManager([255,255,255], [0,0]) #Puts a white VeiwManager on screen
+
+#Set to True for debug print statements
+DEBUG = False
+
 class Fey ( ):
     def __init__ ( self ):
-        pass
-
+        if(DEBUG):print("Initializing");
+        self.RUNNING = True
+        self.DISPLAY_SURF = None
+        self.SIZE = self.WIDTH, self.HEIGHT = 800, 640
+        
     def start ( self ):
+        if(DEBUG):print("Starting");
+        pygame.init()
+        self.DISPLAY_SURF = pygame.display.set_mode(self.SIZE, pygame.DOUBLEBUF)
+        self.DISPLAY_SURF.fill((0,0,0))
+        pygame.display.flip()
+        self.RUNNING = True
+
+
+    def handleEvent(self, event):
+        if(DEBUG):print("Event check");
+        if event.type == pygame.QUIT:
+            self.RUNNING = False
+
+    def update(self):
+        if(DEBUG):print("Updating");
         pass
 
-runner = Fey ( )
-runner.start ( )
+    def render(self):
+        if(DEBUG):print("Rendering");
+        #self.DISPLAY_SURF.blit(v.image, v.rect) Dependant on VeiwManager
+        pass
+
+    def cleanup(self):
+        if(DEBUG):print("Cleaning up");
+        pygame.quit()
+
+    def run(self):
+        if(DEBUG):print("Attempting to run");
+        if self.start() == False:
+            self.RUNNING = False
+
+        if(DEBUG):print("Running");
+        while(self.RUNNING):
+            for event in pygame.event.get():
+                self.handleEvent(event)
+            self.update()
+            self.render()
+        self.cleanup()
+
+if __name__ == "__main__":
+    runner = Fey ( )
+    runner.run ( )
--- ViewManager.py
+++ ViewManager.py
@@ -4,10 +4,20 @@
 #Switches in between them.
 #add pygame stuff here to be initaliazed
 
-class ViewManager ( ):
-    def __init__ ( self ):
+#Some code ripped from: http://www.sacredchao.net/~piman/writing/sprite-tutorial.shtml
+
+import pygame
+
+class ViewManager (pygame.sprite.Sprite):
+    def __init__ ( self, color, initial_position ):
         #pygame
-        pass
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.Surface([30,30])
+        self.image.fill(color)
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = initial_position
 
     def setCurrentView ( self , view ):
         pass
