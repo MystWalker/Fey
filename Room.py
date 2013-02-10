1fb21e946b5e4211d5ec6fc8a38068816046931e
E. Troy Clement <eclement@nmsu.edu>
2013-02-09 19:16:24 -0700

    Fixing Repo
    I don't know what to say. Github shinanagins. This should make things up
    to date. Check comments on "Created ControlManager..." for notes.

--- Room.py
+++ Room.py
@@ -9,12 +9,12 @@ import pygame
 import pygame.sprite
 
 class Room ( ):
-    def __init__ ( self, background):
+    def __init__ ( self, background, avatar):
         self.background = background #Must be a sprite
+        self.avatar = avatar
         self.objects = pygame.sprite.Group()
         pass
 
     #adds an object to the room's group
     def addObject(self, thing):
         self.objects.add(thing)
-
